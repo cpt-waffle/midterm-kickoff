f64292dc7d824f8a7f0b053cbba663f18e1c76dd
vasiliy-klimkin <vasily.klimkin@gmail.com>
2023-10-20 12:30:57 -0400

    add url table

---
+++ db/seeds/02_urls.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO urls (longURL, shortURL) VALUES ('www.google.ca', 'a2DX42');
+INSERT INTO urls (longURL, shortURL) VALUES ('www.reddit.com', 'X23dAl');
+INSERT INTO urls (longURL, shortURL) VALUES ('www.amazon.ca', 'x3DFl1');
